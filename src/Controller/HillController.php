8393687d08f34ee600c5f9f688018d0fd7b2fd25
Edison MASSENA <massenaedison0@outlook.fr>
2020-06-08 10:19:11 +0200

    Ver.1.0

---
+++ src/Controller/HillController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HillController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home()
+    {
+        return $this->render('hill/home.html.twig', [
+            'controller_name' => 'HillController',
+        ]);
+    }
+    
+
+}
