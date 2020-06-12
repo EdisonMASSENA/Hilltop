7202055be00a8186e1b6cebfd1e0d0870b100d00
Edison MASSENA <massenaedison0@outlook.fr>
2020-06-12 12:54:26 +0200

    fixtures

---
+++ src/DataFixtures/MusiqueFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Musique;
+
+class MusiqueFixtures extends Fixture{
+
+    public function load(ObjectManager $manager){
+
+        for($i = 1; $i <= 10; $i++) { 
+            $musique = new Musique();
+
+            $musique->setArtiste("Artiste n°$i") 
+                    ->setAlbum("<p>Album n°$i</p>") 
+                    ->setTitre("Titre");
+
+            $manager->persist($musique);
+        }
+        
+        $manager->flush();
+    }
+}
