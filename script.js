12e7b8965d4b9fdc7d2fb5197559bd7d8b5d376b
EdisonMASSENA <64831993+EdisonMASSENA@users.noreply.github.com>
2020-06-15 17:00:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var btnPopup = document.getElementById('btnPopup');
+var overlay = document.getElementById('overlay');
+
+
+btnPopup.addEventListener('click',openModal);
+
+
+function openModal(){
+    overlay.style.display = 'block';
+}
+
+
+
